7584c07ec516b831046bfa3ea0cf6b89cdef6879
Scott Antipa <scottantipa@gmail.com>
2017-02-08 23:01:03 -0800

    Tighten up spiral, add circle, add some args

--- src/circles.sh
+++ src/circles.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Input: Directory of emojis
-# Output: A .png of a spiral of all the emojis in the input dir
-
-# NOTE: This assumes that emojis are ~100px high and wide and is most optimal for between 50 and 100 emojis
-# NOTE: PLay with END_RADIUS, TRIG_ARG, START_RADIUS for different shaped spirals.
+# Output: A .png of concentric circles of all the emojis in the input dir
 
 # Note that this assumes the user passes dir names with a trailing /
 EMOJI_DIR=$1
@@ -12,13 +9,12 @@ IMAGE_OUT_PATH=$2
 NUM_EMOJIS_IN_PERIOD=$3
 
 echo "Borrowing emojis from ${EMOJI_DIR}"
-echo "And spiraling them into ${IMAGE_OUT_PATH}"
+echo "And making concentric circles with them in ${IMAGE_OUT_PATH}"
 
 TRIG_ARG="(t/${NUM_EMOJIS_IN_PERIOD})*2*pi"
 
-START_RADIUS='200'
-END_RADIUS='(n*20)'
-MULTIPLIER="(${END_RADIUS}*(t/n)+${START_RADIUS})"
+START_RADIUS='300'
+MULTIPLIER="(${START_RADIUS}*floor(t/${NUM_EMOJIS_IN_PERIOD})+${START_RADIUS})"
 
 convert $EMOJI_DIR*.png -set page \
   "+%[fx:${MULTIPLIER}*cos(${TRIG_ARG})]+%[fx:${MULTIPLIER}*sin(${TRIG_ARG})]" \
